28f7328489b4bacbf958b6eeb6cabaae9e439edc
JebobaTea <73501717+JebobaTea@users.noreply.github.com>
2022-04-07 21:51:58 -0400

    You saw nothing.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
-from keepalive import keepalive
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="s!")
 
-keepalive()
 bot.load_extension("cogs.snipe")
 bot.run("TOKEN")
