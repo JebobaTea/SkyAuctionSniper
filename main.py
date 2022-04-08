398562db66598c3a0b9c1a4c4fe1e0b8df7f0f3b
JebobaTea <73501717+JebobaTea@users.noreply.github.com>
2022-04-07 22:08:51 -0400

    Yes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="s!")
+bot = commands.Bot(command_prefix = 's!')
 
-bot.load_extension("cogs.snipe")
-bot.run("TOKEN")
+bot.load_extension('cogs.snipe')
+bot.run('TOKEN')
