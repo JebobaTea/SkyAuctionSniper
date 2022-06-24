60a5fed213a19f73cf064dbc5e28ac9b756b7766
JebobaTea <73501717+JebobaTea@users.noreply.github.com>
2022-06-24 11:37:50 -0400

    Reformat to comply with PEP8

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -4,8 +4,17 @@ import aiohttp
 import re
 from discord.ext import commands, tasks
 
-
-reforges = [" ✦", "⚚ ", " ✪", "✪", "Very Wise ", "Thicc " , "Treacherous ", "Stiff ", "Lucky ", "Jerry's ", "Dirty ", "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ", "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ", "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ", "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ", "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ", "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ", "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ", "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ", "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ", "Superior ", "Unpleasant ", "Zealous "]
+reforges = [" ✦", "⚚ ", " ✪", "✪", "Very Wise ", "Thicc ", "Treacherous ", "Stiff ", "Lucky ", "Jerry's ", "Dirty ",
+            "Fabled ", "Suspicious ", "Gilded ", "Warped ", "Withered ", "Bulky ", "Stellar ", "Heated ", "Ambered ",
+            "Fruitful ", "Magnetic ", "Fleet ", "Mithraic ", "Auspicious ", "Refined ", "Headstrong ", "Precise ",
+            "Spiritual ", "Moil ", "Blessed ", "Toil ", "Bountiful ", "Candied ", "Submerged ", "Reinforced ", "Cubic ",
+            "Warped ", "Undead ", "Ridiculous ", "Necrotic ", "Spiked ", "Jaded ", "Loving ", "Perfect ", "Renowned ",
+            "Giant ", "Empowered ", "Ancient ", "Sweet ", "Silky ", "Bloody ", "Shaded ", "Gentle ", "Odd ", "Fast ",
+            "Fair ", "Epic ", "Sharp ", "Heroic ", "Spicy ", "Legendary ", "Deadly ", "Fine ", "Grand ", "Hasty ",
+            "Neat ", "Rapid ", "Unreal ", "Awkward ", "Rich ", "Clean ", "Fierce ", "Heavy ", "Light ", "Mythic ",
+            "Pure ", "Smart ", "Titanic ", "Wise ", "Bizarre ", "Itchy ", "Ominous ", "Pleasant ", "Pretty ", "Shiny ",
+            "Simple ", "Strange ", "Vivid ", "Godly ", "Demonic ", "Forceful ", "Hurtful ", "Keen ", "Strong ",
+            "Superior ", "Unpleasant ", "Zealous "]
 auc = []
 results = []
 prices = {}
@@ -19,7 +28,7 @@ async def request(url):
         return await resp.json()
 
 
-async def getItem(page = 0, first_page = False):
+async def getItem(page=0, first_page=False):
     global prices
     if first_page:
         prices = {}
@@ -55,7 +64,7 @@ async def getItem(page = 0, first_page = False):
                 elif prices[index][1] > auction['starting_bid'] and prices[index][4] != auction['uuid']:
                     prices[index][1] = auction['starting_bid']
                     prices[index][4] = auction['uuid']
-                    prices[index][5] +=1
+                    prices[index][5] += 1
         else:
             prices[index] = [auction['starting_bid'], float("inf"), auction['item_name'], auction['uuid'], 'UUID', 1]
     return [prices, resp if first_page else {}]
@@ -70,8 +79,8 @@ class Snipe(commands.Cog):
         global channel
         channel = self.bot.get_channel("CHANNEL")
         self.l.start()
-    
-    @tasks.loop(seconds = 61.0)
+
+    @tasks.loop(seconds=61.0)
     async def l(self):
         global auc, results, channel
         results = []
@@ -80,13 +89,16 @@ class Snipe(commands.Cog):
         await asyncio.gather(*[getItem(i) for i in range(1, pages)])
         if auc:
             for auction in auc:
-                if auc[auction][1] > 500000 and auc[auction][0]/auc[auction][1] < 1/10 and auc[auction][1] != float("inf") and auc[auction][1] < 100000000 and auc[auction][5] > 10:
-                    results.append([auc[auction][2], f"{auc[auction][0]:,}", f"{auc[auction][1]:,}", f"{auc[auction][1]-auc[auction][0]:,}", auction])
-            
+                if auc[auction][1] > 500000 and auc[auction][0] / auc[auction][1] < 1 / 10 and auc[auction][1] != float(
+                        "inf") and auc[auction][1] < 100000000 and auc[auction][5] > 10:
+                    results.append([auc[auction][2], f"{auc[auction][0]:,}", f"{auc[auction][1]:,}",
+                                    f"{auc[auction][1] - auc[auction][0]:,}", auction])
+
             for result in results:
-                embed = discord.Embed(color = discord.Color.orange(), title = result[4])
-                embed.add_field(name = result[0], value = "Being sold for " + str(result[1]) + " at a gap of " + str(result[3]) + " \n Second cheapest: " + str(result[2]), inline = False)
-                await channel.send(embed = embed)
+                embed = discord.Embed(color=discord.Color.orange(), title=result[4])
+                embed.add_field(name=result[0], value="Being sold for " + str(result[1]) + " at a gap of " + str(
+                    result[3]) + " \n Second cheapest: " + str(result[2]), inline=False)
+                await channel.send(embed=embed)
 
 
 def setup(bot):
